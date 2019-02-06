74c061fdde598a00fbe196bbc18cc7edf92dfdee
Charlie <charliemsun23@gmail.com>
2019-02-06 09:47:13 -0500

    Spark Basics. Reading and displaying data

--- Basics/spark_basics.py
+++ Basics/spark_basics.py
@@ -8,5 +8,28 @@ df = spark.read.json('Input/people.json')
 # Shows DataFrame
 df.show()
 
+# Shows a DataFrame Statistical Summary
+df.describe().show()
+
 # Shows Columns and DataTypes
 df.printSchema()
+
+# Get specific columns in DataFrame
+df.select(['age']).show()
+
+# Get first row
+df.head()
+
+# Create new column from existing column
+# Note your original df is unchanged
+df.withColumn('new_age', df['age']).show()
+
+# Rename a column
+# Note your original df is unchanged
+df.withColumnRenamed('age', 'new_age').show()
+
+# Create an in memory reference to DataFrame
+# Perform SQL queries on this reference
+df.createOrReplaceTempView('people')
+results = spark.sql('select * from people where age = 30')
+results.show()
